1bacd26853edb61e5fce9c8021e792ac3eb12f2d
GiToon10100011 <Boon10034@gmail.com>
2025-01-21 23:40:03 +0900

    250121 Add CORS Configs

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = import.meta.env.VITE_PORTFOLIO_SERVER_URL;
+axios.defaults.withCredentials = true;
 
 export const fetchComments = async (commentsProject: string) => {
   try {
